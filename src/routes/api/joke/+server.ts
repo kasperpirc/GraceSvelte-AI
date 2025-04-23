2c22755e96fb37a4a0349b1e9d3450deadcd9c1f
LongarMD <mark.longar@gmail.com>
2025-04-23 16:58:58 +0200

    remove

--- src/routes/api/joke/+server.ts
+++ src/routes/api/joke/+server.ts
@@ -1,10 +1,7 @@
-import { joke_message } from '$lib/ai';
 import { json } from '@sveltejs/kit';
 
 export async function POST({ request }) {
 	const message = await request.text();
 
-	const joke = await joke_message.invoke({ topic: message });
-
-	return json({ message: joke });
+	return json({ message: message });
 }
